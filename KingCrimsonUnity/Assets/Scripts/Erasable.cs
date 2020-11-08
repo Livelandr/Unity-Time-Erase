8259ca9f0bb71756cdfd8fc82ae58a293148065c
Livelandr <Livelandr@gmail.com>
2020-11-08 04:03:26 +0300

    First Commit
    Added Project

---
+++ KingCrimsonUnity/Assets/Scripts/Erasable.cs
@@ -0,0 +1,47 @@
+﻿// Made by Livelandr
+// Sounds by David Production (From Jojo's Bizzare Adventure)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Erasable : MonoBehaviour
+{
+
+    public float Xspeed, Yspeed, Zspeed;
+
+    private float StX, StY, StZ;
+
+    void Start()
+    {
+        StX = gameObject.transform.localScale.x; StY = gameObject.transform.localScale.y; StZ = gameObject.transform.localScale.z; // Saving normal sizes'
+
+    }
+
+    public void TEAnim()
+    {
+        StartCoroutine(Animation()); // Calling Animation (From Main.cs)
+    }
+
+    public void OrigScale()
+    {
+        transform.localScale = new Vector3(StX, StY, StZ);      
+    }
+
+
+    IEnumerator Animation() //Animation!
+    {
+        yield return new WaitForSeconds(1f); // Waiting 1 sec...
+        CameraShake.Shake(1, 1); // ShAkE!1!
+        while (gameObject.transform.localScale.x > 0 && gameObject.transform.localScale.y > 0 && gameObject.transform.localScale.y > 0) 
+        {
+            yield return new WaitForSeconds(0.02f); // Small interval
+            gameObject.transform.localScale -= new Vector3(Xspeed, Yspeed, Zspeed); // Reducing object size
+        }
+
+        gameObject.transform.localScale = new Vector3(0, 0, 0); // Object disappear!
+
+
+
+    }
+}
---
+++ KingCrimsonUnity/Assets/Scripts/Main.cs
@@ -0,0 +1,95 @@
+﻿// Made by Livelandr
+// Sounds by David Production (From Jojo's Bizzare Adventure)
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Main : MonoBehaviour
+{
+    private AudioSource Sound;
+
+    public GameObject TEUI;
+    public GameObject TSUI;
+
+    public AudioClip TimeEraseTheme;
+    public AudioClip TimeSkipSound;
+    public AudioClip TimeEraseSound;
+
+    bool isErasing;
+
+    public Material Erasured;
+    public Material Normal;
+
+    // Update is called once per frame
+    void Start()
+    {
+        Sound = GameObject.Find("SoundManager").GetComponent<AudioSource>();
+    }
+
+
+	void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.Q) && !isErasing) // On pressing Q when time doesn't erasing it skips
+        {
+            StartCoroutine(TimeSkip()); // Calling coroutine TimeSkip
+        }
+        if (Input.GetKeyDown(KeyCode.E) && !isErasing) // On pressing E when time doesn't erasing it starts erasing (what?)
+        {
+            StartCoroutine(TimeErase());  // Calling coroutine TimeErase
+        }
+    }
+
+	IEnumerator TimeSkip() // Coroutine TimeSkip
+    {
+        TSUI.SetActive(true); // Red Screen On
+        Time.timeScale = 100f; //Time Accelerate!
+        yield return new WaitForSeconds(0.6f); // Waiting one moment;
+        Time.timeScale = 1f; // Time continues its course
+        Sound.PlayOneShot(TimeSkipSound); // Time skip sound
+        yield return new WaitForSeconds(0.3f); // Waiting...
+        TSUI.SetActive(false); // Red Screen Off
+    }
+
+    IEnumerator TimeErase() // Coroutine
+    {
+
+
+        isErasing = true;
+
+        var objects = FindObjectsOfType<Erasable>(); // Finding all Erasable objects
+        for (var i = 0; i < objects.Length; i++) 
+        {
+            objects[i].GetComponent<Erasable>().TEAnim(); // Calls function TEAnim for all
+        }
+        TEUI.GetComponent<Animator>().SetTrigger("Erase"); // Effect
+        Sound.PlayOneShot(TimeEraseSound); //Time Erase Sound
+        yield return new WaitForSeconds(1); // Waiting 1 sec
+        Sound.PlayOneShot(TimeEraseTheme); //Diavolo's Monolog
+
+        RenderSettings.skybox = Erasured; // Red Sky
+
+        Time.timeScale = 0.5f; // Time Slowdown
+
+        yield return new WaitForSeconds(4.55f); // Waiting 9 seconds
+
+        Time.timeScale = 1f; // Time continues its course    
+
+        Sound.PlayOneShot(TimeEraseSound); // Time Erase sound again
+        TEUI.GetComponent<Animator>().SetTrigger("Erase"); // Effect again
+        yield return new WaitForSeconds(1); // Waiting.......................
+
+        RenderSettings.skybox = Normal; // Return of beautful sky!
+
+        var objectss = FindObjectsOfType<Erasable>(); // finding all erasable objects...
+        for (var i = 0; i < objects.Length; i++)
+        { 
+            objectss[i].GetComponent<Erasable>().OrigScale(); // Returning they into normal state
+        }
+
+
+        isErasing = false;
+
+    }
+}
